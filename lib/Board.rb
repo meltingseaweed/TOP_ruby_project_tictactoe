c986affc42b50d884180b9f285f14dae8e0a2e1e
meltingseaweed <conor.seo@gmail.com>
2026-02-06 06:58:17 +0900

    Remove unused method change

--- lib/Board.rb
+++ lib/Board.rb
@@ -15,17 +15,8 @@ class Board
     p @row2
     p @row3   
   end
-  def change(row, idx, value)
-    case
-    when row == 1 then @row1[idx]=value
-    when row == 2 then @row2[idx]=value
-    when row == 3 then @row3[idx]=value
-    end
-  end
 
-    def check_empty?(position)
-    # Check if the position is empty (thus can play)
-    # or if it is already taken (thus ask another input)
+  def check_empty?(position)
     case position
     when "UL" then @row1[0] == "_"
     when "UC" then @row1[1] == "_"
@@ -39,9 +30,7 @@ class Board
     end
   end
 
-    def place_token(position, player)
-    # Places the X or O in the correct place
-
+  def place_token(position, player)
     case position
     when "UL" then @row1[0] = player
     when "UC" then @row1[1] = player
@@ -55,13 +44,7 @@ class Board
     end
   end
       
-  # Checking sideways is fine. But checking vertical or diagonal
-  # does not work yet.
-  def check_all_outcomes
-    p "Row 1 is: #{@row1}"
-    p "Row 2 is: #{@row2}"
-    p "Row 3 is: #{@row3}"
-    
+  def check_all_outcomes    
     if (@row1 == ["x", "x", "x"]) || (@row1 == ["o", "o", "o"])
       true
     elsif (@row2 == ["x", "x", "x"]) || (@row2 == ["o", "o", "o"])
